af5597ea19db9465adcdb5fa5d3cd40d8b0119f5
adris-h <adrianahanouskova2007@gmail.com>
2026-01-29 13:15:50 +0100

    update fix

--- src/scripts/main.tsx
+++ src/scripts/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import PopUp from "./PopUp.tsx";
 
+import './style.css'
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
